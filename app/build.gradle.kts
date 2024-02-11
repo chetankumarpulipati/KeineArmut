f5299f9b5dc6f32b18eb6b6d15934f1114689b2a
anonymus_user_2991 <147310029+chetankumarpulipati@users.noreply.github.com>
2024-02-11 19:38:08 +0530

    splash screen set successfully

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.solution_challenge.keinearmut"
-        minSdk = 21
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -51,9 +51,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("de.hdodenhof:circleimageview:3.1.0")
-    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
     implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material3:material3:1.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
